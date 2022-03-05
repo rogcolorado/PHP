7fe9e3c89213bd8d4dee3eab6d17f056e5e10464
Rogério Moraes <32040649+rogcolorado@users.noreply.github.com>
2022-03-05 07:23:50 -0300

    FuncoessArrays2

--- funcoesArrays.php
+++ funcoesArrays.php
@@ -38,4 +38,42 @@ print("<h3>Atualizado</h3>");
 echo "<pre>",print_r($index, TRUE),"</pre>";
 
 echo "<pre>",print_r($associativo, TRUE),"</pre>";
+
+if(in_array("AC/DC", $associativo)){
+    echo "<p>Cause I'm back!</p>";
+};
+
+$arrToString = implode(", ",$associativo);
+echo "<p>Eu curto {$arrToString} e muitas outras</p>";
+
+var_dump(explode(", ", $arrToString));
+
+print("<h2>Exemplo prático</h2>");
+
+$profile = [
+    "name" => "Rogério",
+    "company" => "Inovadora",
+    "mail" => "teste@tesste"
+];
+ $template = <<<TPL
+    <article>
+        <h1>{{name}}</h1>
+        <p>{{company}}<br>
+        {{mail}}</p>
+    </article>
+ TPL;
+
+ echo $template;
+ echo str_replace(
+     array_keys($profile), array_values($profile), $template
+ );
+
+ $replaces = "{{".implode("}}&{{", array_keys($profile))."}}";
+
+ echo str_replace(explode("&", $replaces),
+ array_values($profile),
+ $template
+);
+
+ 
 ?>
